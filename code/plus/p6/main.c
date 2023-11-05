429aa0a9d286b7d2474793a0ffb108b3753d84e4
venpy <norbedo@proton.me>
2023-11-05 17:35:49 +0100

    Update

--- code/plus/p6/main.c
+++ code/plus/p6/main.c
@@ -3,8 +3,8 @@
 
 void terne(int a, int b) {
     for (int x = a; x <= b; x++) {
-        for (int y = a; y <= b; y++) {
-            for (int z = a; z <= b; z++) {
+        for (int y = x; y <= b; y++) {
+            for (int z = y; z <= b; z++) {
                 if (x*x + y*y == z*z) printf("%d^2 + %d^2 = %d^2\n", x, y, z);
             }
         }
